16b62158658f5847bbeced7fb327f13b5fc126f2
TKOBabu <rahulrameshbabu16@mittymonarch.com>
2017-05-24 17:52:37 -0500

    Update build_pip_package.sh

--- tensorflow/tools/pip_package/build_pip_package.sh
+++ tensorflow/tools/pip_package/build_pip_package.sh
@@ -157,7 +157,7 @@ function main() {
   pushd ${TMPDIR}
   rm -f MANIFEST
   echo $(date) : "=== Building wheel"
-  "${PYTHON_BIN_PATH:-python}" setup.py bdist_wheel ${GPU_FLAG} >/dev/null
+  "${PYTHON_BIN_PATH:-python3}" setup.py bdist_wheel ${GPU_FLAG} >/dev/null
   mkdir -p ${DEST}
   cp dist/* ${DEST}
   popd
